69c881dc8edc2229549938f0885f3fdea804bbed
Binay Shrestha <Bilavs9@gmail.com>
2019-11-23 12:54:32 +0545

    Updates

---
+++ precache-manifest.125444c6aa376b50af5e82844e25fa53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1650f62f70a5c8039bee91f70fdff03",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "530814021c73bfd3997e",
+    "url": "/robofriends/static/css/main.f3ba0c68.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "530814021c73bfd3997e",
+    "url": "/robofriends/static/js/main.a6e23c2a.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
